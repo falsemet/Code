3f550257333ce9536f597b39232c21f6af57e820
zen <falsemet@gmail.com>
2021-11-20 05:30:17 -0800

    make hello argv

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print ("hello world !")
+import sys
+
+def hello():
+    args=sys.argv
+    if len(args)==1:
+        print ("hello world !")
+    elif len(args)==2:
+        print("hello %s"%args[1])
+    else:
+        print("too many arguments")
+if __name__=="__main__":
+    hello()
